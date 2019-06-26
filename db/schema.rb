e27fd066be7c9c3d3e42c65d3ebce6d856e7c822
OmarAbdallaa <omar.abdallaa@gmail.com>
2019-06-26 16:02:07 +0200

    add cors and axios
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -28,16 +28,5 @@ ActiveRecord::Schema.define(version: 2019_05_27_184334) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
