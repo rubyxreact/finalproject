9d360bdd8184e9c988282eeecefd1237dd97af4c
redha95 <redha955@hotmail.fr>
2019-06-26 17:02:29 +0200

    maj style

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,17 @@ import PostComponent from './components/PostComponent';
 function App() {
   return (
     <div className="App">
-   
+   <header>
+     <div className="logo">
+     ruby<span>x</span>react
+     </div>
+     <div className="menu">
+      <a href="/">Home</a>
+      <a href="/">Articles</a>
+      <a href="/">Mon compte</a>
+      <a href="/">Déconnexion</a>
+     </div>
+   </header>
     <PostComponent />
   </div>
   );
